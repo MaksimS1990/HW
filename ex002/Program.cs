69ee10b43b6e600982f956ef7da1d4efdb8ef491
MaksimS <sipatovgt@mail.ru>
2022-10-16 13:09:01 +0400

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите три числа ");
+
+int count = 0;
+int a = int.Parse(Console.ReadLine()!);
+int max = a;
+int min = a;
+while(count<2)
+{int b = int.Parse(Console.ReadLine()!);
+ if (b>max) max = b;
+ if (b<min) min = b;
+ count++;
+ }
+
+Console.WriteLine("max="+max);
+Console.WriteLine("min="+min);
