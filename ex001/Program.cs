42483b09008c659c517a3bf704d241b0b682d973
MaksimS <sipatovgt@mail.ru>
2022-10-16 12:53:47 +0400

    ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+if (a>b)
+{
+Console.WriteLine();
+Console.WriteLine("max="+a);
+Console.WriteLine("min="+b);
+}
+else
+{
+    Console.WriteLine();
+Console.WriteLine("max="+b);
+Console.WriteLine("min="+a);
+}
