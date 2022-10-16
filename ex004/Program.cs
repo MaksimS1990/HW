7f9276d6297c1f3a4bafacffdaa1a8ab9eab81c6
MaksimS <sipatovgt@mail.ru>
2022-10-16 13:20:45 +0400

    ex004

---
+++ ex004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+if (a<1) Console.WriteLine("Число должно быть больше 1");
+int count = 1;
+int b = 0;
+while (count<=a)
+     {
+          b = (count%2);
+    if (b == 0) Console.Write(count+" ");
+   
+     
+    count++;
+     }
